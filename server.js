047fe954518ebc520cd0b79bff89b7e71be7ff7e
Jose Corredor <jose96corrza@gmail.com>
2021-10-15 01:16:32 -0500

    Cambio ahora se imprime con etiquetas

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world from Express on EC2'))
+app.get('/', (req, res) => res.send('<h1>Express con HTML </h1>'))
 
 app.listen(3000);
 console.log('Server en puerto 3000')
